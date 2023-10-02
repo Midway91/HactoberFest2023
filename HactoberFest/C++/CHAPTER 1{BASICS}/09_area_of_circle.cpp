e7e19e60fbbc03938729d47d299beed752519ba9
Hrishikesh <kynhrishi@gmail.com>
2023-10-02 14:34:19 +0530

    added c++ code for area of circle

---
+++ HactoberFest/C++/CHAPTER 1{BASICS}/09_area_of_circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float area , r , pi;
+    cout<<"Enter Radius of the circle\n";
+    cin>>r;
+    pi = 3.1428;
+    area = (pi*r*r);
+    cout<<"Area Is "<<area;
+    return 0;
+}
