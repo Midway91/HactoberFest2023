a7f5754377f1bbe400a692209d830dac2da0cdd8
Prashant Wadhwa <77116676+prashantwadhwa@users.noreply.github.com>
2023-10-04 23:29:41 +0530

    Create Heap_sort.py

---
+++ Heap_sort.py
@@ -0,0 +1,29 @@
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+# Example usage:
+arr = [64, 34, 25, 12, 22, 11, 90]
+heap_sort(arr)
+print("Sorted array using Heap Sort:", arr)
