e200dd24a6ffb0a7a35aa8180d0bfbc96fddcb59
Ahmad Abu Hasan <36786100+eby8zevin@users.noreply.github.com>
2023-10-08 09:21:04 +0700

    Add DSA BubbleSort in C++

---
+++ cpp/BubbleSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n-1; i++) {
+        swapped = false;
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {                
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = true;
+            }
+        }
+        
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {2, 1, 3, 8, 5, 7, 9, 4, 6, 10}; //change number
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    std::cout << "Array before sorting: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    
+    bubbleSort(arr, n);
+    
+    std::cout << "Array after sorting: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    
+    return 0;
+}
