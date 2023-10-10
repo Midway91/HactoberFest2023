1e283786f7706ec84197b6874e1e592b535225c6
SKSADIRUDDIN <sadiruddin77@gmail.com>
2023-10-10 20:43:54 +0530

    AddedDigitalClockwithAnalog

--- Analog Clock/script.js
+++ Analog Clock/script.js
@@ -21,3 +21,18 @@ function displayTime(){
 }
 
 setInterval(displayTime, 1000);
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    
+    document.getElementById('time').textContent = timeString;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Initial update
+updateClock();
