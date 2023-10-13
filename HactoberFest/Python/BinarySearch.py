793dca65374b449d61baea87f2f6ad67124191c8
Ashel Sherl D Souza <106994839+ashel111@users.noreply.github.com>
2023-10-13 23:24:36 +0530

    Update BinarySearch.py

--- HactoberFest/Python/BinarySearch.py
+++ HactoberFest/Python/BinarySearch.py
@@ -22,3 +22,33 @@ if result != -1:
     print(f"Target {target} found at index {result}")
 else:
     print(f"Target {target} not found in the array")
+
+
+
+
+
+#mycode
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2 
+
+        if arr[mid] == target:
+            return mid  
+        elif arr[mid] < target:
+            left = mid + 1 
+        else:
+            right = mid - 1  
+
+    return -1 
+arr = [1, 3, 5, 7, 9, 11, 13, 15]
+target = 7
+result = binary_search(arr, target)
+
+if result != -1:
+    print(f"Target {target} found at index {result}")
+else:
+    print(f"Target {target} not found in the list.")
+
