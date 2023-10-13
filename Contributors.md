1d7ebec10505bd896ea9d2caf29b14ad02e7c167
redacherkaoui <redacherkaoui67@gmail.com>
2023-10-13 20:40:47 +0100

    Array.sol Boundary checks and error message && Contributors.md

--- Contributors.md
+++ Contributors.md
@@ -8,6 +8,7 @@ ABHINANDAN BARUA
 Frank Edu
 seabeePraveen
 Pranav
+CHERKAOUI REDA ANOAR (https://github.com/redacherkaoui/HactoberFest2023)
 mohamed mahfouz
 Mohamad Shukry
 joy moral
--- solidity/Array.sol
+++ solidity/Array.sol
@@ -1,15 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.8.2 <0.9.0;
-contract Array
-{
-    uint[4] public arr=[10,20,30,40];
-    function setter(uint index,uint value) public 
-    {
-        arr[index]=value;
+
+contract Array {
+    uint[4] public arr = [10, 20, 30, 40];
+
+    function setter(uint index, uint value) public {
+        require(index < arr.length, "Index out of bounds");
+        arr[index] = value;
     }
-    function length() public view  returns(uint)
-    {
-       return  arr.length;
+
+    function length() public view returns (uint) {
+        return arr.length;
+    }
+
+    function getValueAt(uint index) public view returns (uint) {
+        require(index < arr.length, "Index out of bounds");
+        return arr[index];
     }
 }
