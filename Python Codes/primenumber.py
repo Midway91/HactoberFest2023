83b9e9711d5f1195b9a9f0aa754a4d64d8a82b73
Chirag Verma <cv091998@gmail.com>
2023-10-15 19:52:45 +0530

    Python folder: added primenumber check

---
+++ Python Codes/primenumber.py
@@ -0,0 +1,11 @@
+num=int(input("Enter a number: "))
+flag =False
+if num>1:
+    for i in range(2,num):
+        if(num%i)==0:
+            flag = True
+            break
+if flag:
+    print(num,"Is not a prime number")
+else:
+    print(num,"is a prime number")
