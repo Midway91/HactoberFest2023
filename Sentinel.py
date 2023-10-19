1ff525ec0eff20b06438f2bd58275915270b37eb
Aditya Singh <118094804+08042004Aditysingh@users.noreply.github.com>
2023-10-19 10:29:15 +0530

    Create Sentinel.py

---
+++ Sentinel.py
@@ -0,0 +1,40 @@
+// C++ implementation of the approach
+#include <iostream>
+using namespace std;
+
+// Function to search x in the given array
+void sentinelSearch(int arr[], int n, int key)
+{
+
+	// Last element of the array
+	int last = arr[n - 1];
+
+	// Element to be searched is
+	// placed at the last index
+	arr[n - 1] = key;
+	int i = 0;
+
+	while (arr[i] != key)
+		i++;
+
+	// Put the last element back
+	arr[n - 1] = last;
+
+	if ((i < n - 1) || (arr[n - 1] == key))
+		cout << key << " is present at index " << i;
+	else
+		cout << "Element Not found";
+}
+
+// Driver code
+int main()
+{
+	int arr[] = { 10, 20, 180, 30, 60, 50, 110, 100, 70 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int key = 180;
+
+	sentinelSearch(arr, n, key);
+
+	return 0;
+}
+// This code is contributed by Mandeep Dalavi
