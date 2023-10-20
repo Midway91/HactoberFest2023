a984dafabb47ba0a3c8239720d0edc9a88226c64
pavanpatchikarla <76902617+P-1000@users.noreply.github.com>
2023-10-20 22:26:04 +0530

    binary search

---
+++ Java/BinarySearch/BinaraySearch.java
@@ -0,0 +1,26 @@
+public class BinarySearch {
+    int binarySearch(int arr[], int x) {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (arr[m] == x)
+                return m;
+            if (arr[m] < x)
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+        return -1;
+    }
+    public static void main(String args[]) {
+        BinarySearch ob = new BinarySearch();
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int n = arr.length;
+        int x = 10;
+        int result = ob.binarySearch(arr, x);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index " + result);
+    }
+}
