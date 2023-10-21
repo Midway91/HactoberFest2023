d2992a17838a41d529d977bfa93e58306d5dd7d8
Shubhankar_Ranjan <shubhankar515@gmail.com>
2023-10-22 00:25:04 +0530

    fixing

--- Hacktoberfest-2023/Programming/PYTHON/Coin_flip.py
+++
@@ -1,8 +0,0 @@
-import random
-def coin_flip():
-	return random.choice(['heads', 'tails'])
-
-if coin_flip() == 'heads':
-	print('Heads - You win!')
-else:
-	print('Tails - You lose!')
--- Hacktoberfest-2023/Programming/PYTHON/SQL 50 solutions Leetcode/1068.sql
+++
@@ -1,8 +0,0 @@
-    
-select p.product_name, s.year, s.price
-
-from sales s
-
-left join product p
-
-on s.product_id=p.product_id;
