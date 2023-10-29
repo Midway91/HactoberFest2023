70cd3fe50b134017f27415afeb500ec450be9bbb
Mohamed Shafook Shafni Ahmed <105093382+sfn1024@users.noreply.github.com>
2023-10-29 21:36:33 +0530

    Create leapYear.py

---
+++ leapYear.py
@@ -0,0 +1,12 @@
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+year = int(input("Enter a year: "))
+
+if is_leap_year(year):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
